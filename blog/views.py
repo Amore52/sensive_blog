bb5073b4ad07b116409fc58f78eb400301f932d7
Amore52 <4more52@gmail.com>
2024-11-29 12:55:38 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
-from blog.models import Comment, Post, Tag
+from blog.models import Post, Tag
 
 
 def serialize_post(post):
